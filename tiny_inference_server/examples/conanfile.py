0d855b8ec0ac18de55aca8a94c641d6d51a2242c
Stefano Lusardi <lusardi.stefano@gmail.com>
2022-03-19 01:46:30 +0100

    Updated opencv to 4.5.5

--- tiny_inference_server/examples/conanfile.py
+++ tiny_inference_server/examples/conanfile.py
@@ -3,4 +3,4 @@ from conans import ConanFile, CMake
 class TinyInfereneceEngine_ServerExamples_Conan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake_find_package"
-    requires = "opencv/4.5.3"
+    requires = "opencv/4.5.5"
